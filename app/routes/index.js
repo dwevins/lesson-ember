f9e9d9cc7655ee7c8a328098d2c98c12c626565a
Danny Evins <dcastkid@gmail.com>
2016-07-19 11:05:26 -0500

    model has multiple objects

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,9 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return {
-      img: 'http://placecage.com/300/300',
-      title: 'I am an object'
-  };
+    return [
+      {
+        img: 'http://placecage.com/300/300',
+        title: 'I am an object'
+      },
+      {
+        img: 'http://stevensegallery.com/400/400',
+        title: 'segal'
+      },
+    ]
   }
 });
